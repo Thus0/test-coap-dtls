1023127b461deefa0b32d2b100469c450b0b3fba
Thus0 <thus0@free.fr>
2022-01-31 17:38:28 +0100

    examples with psk, rpk, x509 in coaps client

---
+++ californium/client/client-coap-dtls-cert-californium.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# description: COAPS client with californium framework
+# environment variables:
+#   - DTLS_SERVER (default: 192.168.11.105)
+#   - DTLS_PORT   (default: 5684)
+#
+#        author: Thus0
+# last modified: 2022-01-31 17:36
+
+# Exit on first error
+set -e
+
+# Default environment variables
+[ -z ${DTLS_SERVER} ] && DTLS_SERVER=192.168.11.105
+[ -z ${DTLS_PORT} ] && DTLS_PORT=5684
+
+# Configuration
+CALIFORNIUM_DIR="/app/californium"
+COAPS_CLIENT_DIR="${CALIFORNIUM_DIR}/demo-apps/cf-secure/target"
+COAPS_CLIENT_JAR="cf-secure-3.3.0-SNAPSHOT.jar"
+
+# Client arguments
+
+java -jar ${COAPS_CLIENT_DIR}/${COAPS_CLIENT_JAR} SecureClient X509
+
