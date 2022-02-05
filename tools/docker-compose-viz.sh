ad5771872a0a2f2deef7c0c7d4f7802bd225fc34
Thus0 <thus0@free.fr>
2022-02-05 08:25:29 +0100

    docker-compose-viz example

---
+++ tools/docker-compose-viz.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# description: convert docker-compose.yml to graph picture
+#
+# author: Thus0
+# last modified: 2022-02-05 08:23
+
+winpty docker run --rm -it --name dcv \
+	-v $(pwd):/input \
+	pmsipilot/docker-compose-viz render \
+       	--no-volumes --no-ports --no-networks
+	-m image --force ../docker-compose.yml 
