d4748417ad748513949914f3a91950900b1a2bb4
Thus0 <thus0@free.fr>
2022-02-05 19:52:46 +0100

    path tinydtls path

--- freecoap/files/build-freecoap.sh
+++ freecoap/files/build-freecoap.sh
@@ -2,7 +2,7 @@
 # description: build and install FreeCoAP
 #
 #        author: Thus0
-# last modified: 2022-02-05 09:15
+# last modified: 2022-02-05 19:52
 
 # Exit on undefined variable and first error
 set -u
@@ -24,11 +24,14 @@ cp /app/configure.ac /app/freecoap-tinydtls/configure.ac
 cp /app/Makefile.am /app/freecoap-tinydtls/Makefile.am
 cp /app/lib/Makefile.am /app/freecoap-tinydtls/lib/Makefile.am
 
-# FIXME: Dirty hack so tinydtls headers are found
-mv /usr/local/include/tinydtls/* /usr/local/include
-
 # Build and install FreeCoAP
 cd /app/freecoap-tinydtls || exit 1
+sed -i -e 's|#include "tinydtls.h"|#include "tinydtls/tinydtls.h"|' lib/include/coap_client.h
+sed -i -e 's|#include "tinydtls.h"|#include "tinydtls/tinydtls.h"|' lib/include/coap_server.h
+sed -i -e 's|#include "dtls.h"|#include "tinydtls/dtls.h"|' lib/include/coap_client.h
+sed -i -e 's|#include "dtls.h"|#include "tinydtls/dtls.h"|' lib/include/coap_server.h
+sed -i -e 's|#include "dtls_debug.h"|#include "tinydtls/dtls_debug.h"|' lib/src/coap_client.c
+sed -i -e 's|#include "dtls_debug.h"|#include "tinydtls/dtls_debug.h"|' lib/src/coap_server.c
 autoreconf --install
 ./configure
 make
